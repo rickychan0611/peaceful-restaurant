d03643f615617855a6297f0067e28315798d4562
rickychan0611 <ric0611@gmail.com>
2021-06-13 00:20:43 -0700

    docker

--- next.config.js
+++ next.config.js
@@ -1,18 +1,21 @@
-// const nextTranslate = require('next-translate')
+const nextTranslate = require('next-translate')
 
-// module.exports = nextTranslate()
+module.exports = nextTranslate()
 
-// module.exports = nextTranslate({
+module.exports = nextTranslate({
+  experimental: {
+    scrollRestoration: true
+  },
+  future: {
+        webpack5: false,
+      },
+})
+
+// module.exports = {
+//   future: {
+//     webpack5: false,
+//   },
 //   experimental: {
 //     scrollRestoration: true
 //   }
-// })
-
-module.exports = {
-  future: {
-    webpack5: false,
-  },
-  experimental: {
-    scrollRestoration: true
-  }
-}
+// }
