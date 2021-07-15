bd9e9338c873f46ea94f2c6d34d067d213e75406
rickychan0611 <ric0611@gmail.com>
2021-07-15 09:52:21 -0700

    implement sever time

--- util/useCurrentTime.js
+++ util/useCurrentTime.js
@@ -7,9 +7,8 @@ const useCurrentTime = () => {
 
   useEffect(async () => {
     const getCurrentTime = await axios.get(process.env.NEXT_PUBLIC_HOST_URL + "/api/gettime")
-    // console.log("time", getCurrentTime.data.data)
-    // setCurrentTime(moment(getCurrentTime.data.data))
-    setCurrentTime(moment("2021-07-18 22:32:58"))
+    setCurrentTime(moment(getCurrentTime.data.data))
+    // setCurrentTime(moment("2021-07-18 22:32:58"))
   }, []);
 
   return currentTime
